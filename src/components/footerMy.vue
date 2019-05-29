b852b6dd6afc07f8516ac75f35f0a2032fc39a08
Andriy Statkevych <astudio777@gmail.com>
2019-05-29 15:48:53 +0300

    modified:   src/components/footerMy.vue

--- src/components/footerMy.vue
+++ src/components/footerMy.vue
@@ -2,7 +2,7 @@
     <div id="footer" class=" bg-blue-darkest text-white" >
         <div class="container mx-auto flex text-center items-top justify-center flex-wrap py-10">
              <h2 class="border-b-2 border-grey-darker pb-1 mb-16 text-4xl w-full text-left">Call Now!</h2>
-            
+            <!--
             <div class="lg:w-1/3 w-1/2 ">
                 <p class=" lg:text-2xl text-blue">
                     <a href="tel:212-233-2830"><g-image class="p-3 rounded-full mb-3 bg-blue" src="~/assets/img/phone-icon-inv.png" width="70" /></a>
@@ -17,12 +17,12 @@
                     <a class=" text-white text-xl" href="tel:212-302-1848">212-302-1848</a>
                 </p>
             </div>
-        
+            -->
             <div class="lg:w-1/3 w-1/2 mt-8 lg:mt-0">
                 <p class=" lg:text-2xl text-blue">
                     <a href="tel:516-295-4867"><g-image class="p-3 rounded-full mb-3 bg-blue" src="~/assets/img/phone-icon-inv.png" width="70" /></a>
                     <br>
-                    <span class="block xs:text-2xl ">Island Office</span>
+                    <span class="block xs:text-2xl ">LONG ISLAND</span>
                     <a class=" text-white text-xl" href="tel:516-295-4867">516-295-4867</a>
                 </p>
             </div>
